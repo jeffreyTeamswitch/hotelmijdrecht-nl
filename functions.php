e94905d7ac44adaaae3fd3a4009a8cc8969f43bd
jeffreyTeamswitch <jeffrey@teamswitch.nl>
2023-05-23 11:33:15 +0200

    innit

--- functions.php
+++ functions.php
@@ -1,3 +1,45 @@
 <?php
 
+$child_blocks = array(
+    'rooms',
+);
+
+// add google font
+function add_google_fonts() {
+    wp_enqueue_style( 'Fira Sans', 'https://fonts.googleapis.com/css2?family=Fira+Sans:wght@300;400;700;900&display=swap', false );
+    wp_enqueue_style( 'Libre Baskerville', 'https://fonts.googleapis.com/css2?family=Libre+Baskerville:wght@400;700&display=swap', false );
+}
+add_action( 'wp_enqueue_scripts', 'add_google_fonts' );
+
+// register custom blocks
+
+add_action('init', 'register_child_acf_blocks', 5);
+
+function register_child_acf_blocks() {
+
+    global $child_blocks;
+
+    foreach ($child_blocks as $block) {
+        // console_log(register_block_type( get_theme_file_path( 'partials/blocks/' . $block ) ));
+        register_block_type( get_theme_file_path( 'partials/blocks/' . $block ) );
+    }
+}
+
+// add custom blocks to whitelist
+
+add_filter( 'allowed_block_types_all', 'switch_child_allowed_block_types', 20, 2 );
+function switch_child_allowed_block_types( $allowed_blocks ) {
+
+    global $child_blocks;
+
+    // $allowed_blocks = array();
+
+    foreach ($child_blocks as $block) {
+        $allowed_blocks[] = 'switch/' . $block;
+    }
+
+    console_log($allowed_blocks);
+
+    return $allowed_blocks;
+}
 ?>
---
+++ partials/blocks/rooms/rooms.php
@@ -0,0 +1,148 @@
+<?php 
+
+// vars
+
+$card_type = get_field('card_type');
+$align = get_field('align');
+$background = get_field('background');
+$color = get_field('color');
+$id = get_field('id');
+$image = get_field('image');
+$style = get_field('style');
+
+?>
+
+<section id="<?= $id; ?>" class="cards card--<?= $align; ?> card--<?= $background; ?> card--<?= $color; ?> card--<?= $image; ?> card--<?= $style; ?>">
+	<div class="wrapper">
+			
+	<?php
+		$post_type = 'rooms';
+		$post_per_page = get_field('card_amount');
+		$post_more = get_field('card_more');
+		$post_current_page = (get_query_var('paged')) ? get_query_var('paged') : 1;
+		$post_query = new WP_Query(
+			array(  
+				'post_type' => $post_type,
+				'post_status' => 'publish',
+				'orderby' => array(
+					'post_date' => 'desc', 
+				),
+				'posts_per_page' => $post_per_page,
+				'paged' => $post_current_page
+			)
+		);
+		$post_max_pages = $post_query->max_num_pages;
+		$post_text_more = 'Laad meer';
+		$post_text_loading = 'Aan het laden ...';
+		$post_text_done = 'Uitgeladen';
+
+		?>
+
+		<div class="card__container" 
+			
+			data-post-type="<?= $post_type; ?>"
+			data-post-per-page="<?= $post_per_page; ?>"
+			data-post-max-pages="<?= $post_max_pages; ?>"
+			data-post-text-more="<?= $post_text_more; ?>"
+			data-post-text-loading="<?= $post_text_loading; ?>"
+			data-post-text-done="<?= $post_text_done; ?>"
+		
+		>
+
+			<?php
+				
+			while ( $post_query->have_posts() ) : $post_query->the_post();
+
+				// vars
+
+				$id = get_the_ID();
+
+				$type = '';
+				$image = get_the_post_thumbnail_url($id, '960-1-1');
+				$title = get_the_title($id);
+				$text = false;
+				$button = get_the_permalink($id);
+
+				?>
+
+				<div class="card__item">
+
+					<?php if ($image): ?>
+
+						<div class="card__image <?php if ($type == 'video'): ?>card__image--video<?php endif; ?>">
+
+							<a href="<?= $button; ?>" <?php if ($post_type == 'facebook' || $post_type == 'projects' ): ?>target="_blank"<?php endif; ?>>
+
+								<img loading="lazy" src="<?= $image; ?>" alt="<?= $title; ?>">
+							
+							</a>
+
+						</div>
+
+					<?php endif; ?>
+
+					<?php if ($title): ?>
+
+						<div class="card__text">
+
+							<h2><?= $title; ?></h2>
+
+							<?php if ($post_type == 'projects'): ?>
+
+								<h3><?= $date; ?></h3>
+								
+							<?php endif; ?>
+
+							<?php if ($text): ?>
+
+								<p><?= $text; ?></p>
+
+							<?php endif; ?>
+						</div>
+
+					<?php endif; ?>
+
+					<?php if ($button): ?>
+
+						<div class="card__button">
+
+							<a class="button button--filled-secondary" href="<?= $button; ?>" <?php if ($post_type == 'facebook' || $post_type == 'projects' ): ?>target="_blank"<?php endif; ?>>Bekijk</a>
+
+						</div>
+
+					<?php endif; ?>
+
+				</div>
+
+				<?php
+
+			endwhile;
+
+			?>
+
+		</div>
+
+		<?php 
+		
+		if ($post_more): 
+		
+			?>
+
+			<div class="card__load-more">
+
+				<button class="button button--filled-secondary <?= ($post_max_pages == 1) ? 'button--disabled' : '' ; ?>" <?= ($post_max_pages == 1) ? 'disabled' : '' ; ?>><?= ($post_max_pages == 1) ? $post_text_done : $post_text_more ; ?></button>
+
+			</div>
+
+			<?php 
+		
+		endif;
+
+		wp_reset_postdata();
+		break;
+	
+
+	?>
+	</div>
+</section>
+
