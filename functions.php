5ac6b535909c1746a6a35e9b75d474534e6bfaa0
jeffreyTeamswitch <jeffrey@teamswitch.nl>
2023-05-26 14:31:57 +0200

    Price pop-up

--- functions.php
+++ functions.php
@@ -8,6 +8,12 @@
     }
 //
 // Scripts
+function add_ratebox() {
+    wp_enqueue_script('ratebox', 'https://static.cubilis.eu/js/ratebox.bundle.js');
+    wp_enqueue_script('child-main', get_stylesheet_directory_uri() . '/js/main.js' , array('ratebox'), wp_get_theme()->get('Version') );
+}
+
+add_action( 'wp_enqueue_scripts', 'add_ratebox' );
 //
 // Custom post type
     $child_blocks = array(
---
+++ js/main.js
@@ -0,0 +1,1 @@
+var rb = new Ratebox({IBEUrl: 'hampshire-hotel-mijdrecht', key: '79ACA542-6611-4E9F-AD3A-D820868E97F6', locale: 'en' });
