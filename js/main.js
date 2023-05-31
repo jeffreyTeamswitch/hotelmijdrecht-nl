20aafa4c34d6a567012486ffe0f88b1e30bc3086
jeffreyTeamswitch <jeffrey@teamswitch.nl>
2023-05-31 13:15:10 +0200

    changed the booking form

--- js/main.js
+++ js/main.js
@@ -1,7 +1,8 @@
-jQuery(function ($) {
-	
 
-	$(window).scroll(function () {
-		var rb = new Ratebox({IBEUrl: 'hampshire-hotel-mijdrecht', key: '79ACA542-6611-4E9F-AD3A-D820868E97F6', locale: 'en' });
-	});
-});
+console.log('hallo world');
+var rb = new Ratebox({IBEUrl: 'hampshire-hotel-mijdrecht', key: '79ACA542-6611-4E9F-AD3A-D820868E97F6', locale: 'en' })
+// jQuery(function ($) {
+
+// 	$(window).scroll(function () {
+// 	});
+// });
